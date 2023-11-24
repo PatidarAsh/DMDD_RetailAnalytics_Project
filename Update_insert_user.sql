473916233424e42f62eecd7adb3fd542a570842f
TanujaMahajan17 <145176188+TanujaMahajan17@users.noreply.github.com>
2023-11-24 13:17:56 -0500

    Updating file

--- Update_insert_user.sql
+++ Update_insert_user.sql
@@ -14,7 +14,11 @@ BEGIN
         INSERT INTO USERS(User_ID, User_Name, User_Address, User_Phone)
         VALUES (USER_ID_SEQ.NEXTVAL, p_user_name, p_user_address, p_user_phone);
         DBMS_OUTPUT.PUT_LINE('User details added.');
-        
+        ELSE 
+        SELECT COUNT(*) INTO v_count 
+        FROM USERS 
+        WHERE User_Name = p_user_name
+        AND (User_Address <> p_user_address OR User_Phone <> p_user_phone);
 
    
         END IF;
