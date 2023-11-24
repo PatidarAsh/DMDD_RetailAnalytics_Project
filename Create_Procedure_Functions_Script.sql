da679d285ec7bb8b27aa7c39d3c4eb7cf76970c8
PatidarAsh <113142513+PatidarAsh@users.noreply.github.com>
2023-11-24 14:05:50 -0500

    adding file to store procedure and functions

---
+++ Create_Procedure_Functions_Script.sql
@@ -0,0 +1,38 @@
+
+
+
+--3.--ADD_TO_CART_ITEMS
+--This procedure takes input from customer like : Name, Product Name, Quantity, MOde_of_payment, whether he/she wants to check out their cart items or not 
+--First it checks if there is any active cart againts a user , if found then insert data in that one 
+-- if not found then creates a new entry 
+
+CREATE OR REPLACE PROCEDURE ADD_TO_CART_ITEMS(
+    PI_USER_NAME VARCHAR2,
+    PI_PRODUCT_NAME VARCHAR2,
+    PI_QUANTITY NUMBER,
+    PI_MODE_OF_PAYMENT VARCHAR2,
+    PI_IS_CHECKED_OUT CHAR
+)
+AS
+    V_Cart_ID CART.CART_ID%TYPE;
+    V_User_ID USERS.User_id%TYPE;
+BEGIN
+    -- Check if there's an active cart for the user
+    BEGIN
+        SELECT Cart_ID, User_id
+        INTO V_Cart_ID, V_User_ID
+        FROM USER_CART_VIEW
+        WHERE User_NAME = PI_USER_NAME;
+        
+       
+        
+        DBMS_OUTPUT.PUT_LINE('USER_ID: ' || V_User_ID);
+        
+    
+    END;
+END ADD_TO_CART_ITEMS;
+/
+
+
+
+
