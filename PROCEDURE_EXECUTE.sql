1696b3824075537b8fa5d2888ea962fc9ae91693
TanujaMahajan17 <145176188+TanujaMahajan17@users.noreply.github.com>
2023-11-27 11:54:29 -0500

    adding execute statements for project

---
+++ PROCEDURE_EXECUTE.sql
@@ -0,0 +1,63 @@
+EXECUTE UPDATE_OR_INSERT_USER('John Doe', '123 Main Street', '123-456-7890');
+
+EXECUTE UPDATE_OR_INSERT_USER('Jane Smith', '456 Elm Street', '987-654-3210');
+
+EXECUTE UPDATE_OR_INSERT_USER('Mike Johnson', '789 Elm Lane', '111-222-3333');
+
+EXECUTE UPDATE_OR_INSERT_USER('Emily Brown', '567 Pine Road', '444-555-6666');
+
+EXECUTE UPDATE_OR_INSERT_USER('Chris Williams', '890 Cedar Drive', '777-888-9999');
+
+EXECUTE UPDATE_OR_INSERT_USER('SarahWilson', '234 Birch Street', '333-222-1111');
+
+EXECUTE UPDATE_OR_INSERT_USER('Alex Clark', '901 Spruce Avenue', '666-777-8888');
+
+EXECUTE UPDATE_OR_INSERT_USER('Ava Martinez ', '345 Walnut Lane', '999-888-7777');
+
+EXECUTE UPDATE_OR_INSERT_USER('Daniel Taylor', '678 Maple Road', '222-333-4444');
+
+EXECUTE UPDATE_OR_INSERT_USER('Olivia Anderson', '123 Oak Street', '555-444-3333');
+
+EXECUTE UPDATE_OR_INSERT_USER('Alice Johnson', '456 Oak Avenue', '987-654-3210');
+
+EXECUTE UPDATE_OR_INSERT_USER('Bob Smith', '789 Pine Lane', '456-789-0123');
+
+EXECUTE UPDATE_OR_INSERT_USER('Emily White', '321 Cedar Road', '789-012-3456');
+
+EXECUTE UPDATE_OR_INSERT_USER('David Brown', '567 Elm Street', '012-345-6789');
+
+EXECUTE UPDATE_OR_INSERT_USER('Grace Miller', '234 Birch Lane', '345-678-9012');
+
+EXECUTE UPDATE_OR_INSERT_USER('Thomas Wilson', '890 Maple Road', '678-901-2345');
+
+EXECUTE UPDATE_OR_INSERT_USER('Sophia Davis', '432 Pine Avenue', '901-234-5678');
+
+EXECUTE UPDATE_OR_INSERT_USER('Michael Johnson', '876 Cedar Lane', '234-567-8901');
+
+EXECUTE UPDATE_OR_INSERT_USER('Olivia Taylor', '109 Birch Road', '567-890-1234');
+
+EXECUTE UPDATE_OR_INSERT_USER('Henry Thomas', '543 Oak Street', '890-123-4567');
+
+EXECUTE UPDATE_OR_INSERT_USER('Emma Martinez', '876 Maple Avenue', '123-456-7890');
+
+EXECUTE UPDATE_OR_INSERT_USER('Christopher Hall', '210 Pine Lane', '456-789-0123');
+
+EXECUTE UPDATE_OR_INSERT_USER('Ava Jackson', '654 Cedar Road', '789-012-3456');
+
+EXECUTE UPDATE_OR_INSERT_USER('Daniel White', '987 Elm Street', '012-345-6789');
+
+EXECUTE UPDATE_OR_INSERT_USER('Lily Brown', '321 Birch Lane', '345-678-9012');
+
+EXECUTE UPDATE_OR_INSERT_USER('Andrew Miller', '765 Maple Road', '678-901-2345');
+
+EXECUTE UPDATE_OR_INSERT_USER('Mia Wilson', '098 Pine Avenue', '901-234-5678');
+
+EXECUTE UPDATE_OR_INSERT_USER('Ethan Davis', '543 Cedar Lane', '234-567-8901');
+
+EXECUTE UPDATE_OR_INSERT_USER('Grace Johnson', '876 Birch Road', '567-890-1234');
+
+EXECUTE UPDATE_OR_INSERT_USER('Ryan Taylor', '109 Oak Street', '890-123-4567');
+
+EXECUTE UPDATE_OR_INSERT_USER('Ashvini', 'forest Hills', '987-654-3218');
+
+
