b5fba3f09ee11d635acb78627ec9bab7888e504b
RitwikGiri98 <144959022+RitwikGiri98@users.noreply.github.com>
2023-12-04 12:32:01 -0500

    Added Trigger for UserName Display

--- Create_Package_Trigger.sql
+++ Create_Package_Trigger.sql
@@ -1,1 +1,17 @@
 -- add Triggers and Packeges to this file 
+
+
+CREATE OR REPLACE PACKAGE TriggerPackage AS
+  v_user_name USERS.USER_NAME%TYPE;
+END TriggerPackage;
+/
+
+CREATE OR REPLACE TRIGGER ViewUserName
+AFTER INSERT OR UPDATE ON USERS
+FOR EACH ROW
+ENABLE
+BEGIN
+  TriggerPackage.v_user_name := :NEW.USER_NAME;
+  DBMS_OUTPUT.PUT_LINE('User Name: ' || TriggerPackage.v_user_name);
+END;
+/
